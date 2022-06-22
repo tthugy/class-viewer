fdf27728c381ef39f92665036b09eee2c321bcd3
tthugy <supermanace7@gmail.com>
2022-06-22 09:49:44 +0900

    update

--- class-viewer/pages/menu1.tsx
+++ class-viewer/pages/menu1.tsx
@@ -2,15 +2,15 @@ import type { NextPage } from 'next'
 import Nav from './nav'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/index.module.css'
+import styles from '../styles/menu1.module.css'
 
-const Home: NextPage = () => {
+const Menu1: NextPage = () => {
   return (
     <div>
-      <span className={styles.center}>Next App @Home</span>
+      <span className={styles.center}>Next App @Menu1</span>
       <Nav/>
     </div>
   )
 }
 
-export default Home
+export default Menu1
