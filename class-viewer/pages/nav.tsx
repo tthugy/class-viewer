5aa670714516ee09b3414393c4aebb4532a960e4
tthugy <supermanace7@gmail.com>
2022-06-22 09:51:44 +0900

    update

--- class-viewer/pages/nav.tsx
+++ class-viewer/pages/nav.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import styles from '../styles/nav.module.css'
 
 const Nav: NextPage = () => {
     return (
         <ul className={styles.nav}>
-            <li>Home</li>
-            <li>Menu1</li>
+            <Link href="/">
+                <li>Home</li>
+            </Link>
+            <Link href="/menu1">
+                <li>Menu1</li>
+            </Link>
             <li>Menu2</li>
             <li>Menu3</li>
         </ul>
