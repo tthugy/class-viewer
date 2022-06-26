e4235690021baa1214f906cec210336360dd664c
tthugy <supermanace7@gmail.com>
2022-06-26 22:59:59 +0900

    update

--- class-viewer/pages/header.tsx
+++ class-viewer/pages/header.tsx
@@ -1,13 +1,27 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import styles from '../styles/header.module.css'
+import cn from 'classnames';
+
+const handleClick = () => {
+    const navBox = document.querySelector("#navWrap");
+    const style = cn.bind(styles);
+    navBox.classList.add('open');
+}
 
 const Header: NextPage = () => {
     return (
-        <header className={styles.header}>
-            <i className="bi bi-list"></i>
-            <span>MyApp</span>
-        </header>
+        <div>
+            <header className={styles.header}>
+                <i onClick={handleClick} className="bi bi-list"></i>
+                <span>MyApp</span>
+            </header>
+            <div id="navWrap" className={styles.navigator_wrap}>
+                <div className={styles.navigation}>
+
+                </div>
+            </div>
+        </div>
     )
   }
   
