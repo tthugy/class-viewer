154a892ffb2e779cabeba59f6bf55960482c8313
tthugy <supermanace7@gmail.com>
2022-07-02 22:07:17 +0900

    update

--- class-viewer/pages/nav.tsx
+++ class-viewer/pages/nav.tsx
@@ -8,21 +8,25 @@ const Nav: NextPage = () => {
             <Link href="/">
                 <li>
                     <i className="bi bi-house-door-fill"></i>
+                    <span>홈</span>
                 </li>
             </Link>
             <Link href="/menu1">
                 <li>
                     <i className="bi bi-chat"></i>
+                    <span>채팅</span>
                 </li>
             </Link>
             <Link href="/menu2">
                 <li>
                     <i className="bi bi-calendar-check"></i>
+                    <span>일정관리</span>
                 </li>
             </Link>
             <Link href="/menu3">
                 <li>
                     <i className="bi bi-clock"></i>
+                    <span>홈</span>
                 </li>
             </Link>
         </ul>
