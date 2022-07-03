a3e84a05e0ee0303b65f588bfe95cf280a502794
tthugy <supermanace7@gmail.com>
2022-07-03 22:50:43 +0900

    update

---
+++ class-viewer/pages/home.tsx
@@ -0,0 +1,49 @@
+import type { NextPage } from 'next'
+import Header from './header'
+import Image from 'next/image'
+import styles from '../styles/home.module.css'
+import toast, { Toaster } from 'react-hot-toast'
+import React, { useState, useEffect } from 'react'
+
+const Home: NextPage = () => {
+
+  const [state, setState] = useState(false);
+
+  const notify = () => {
+    if(state === false){
+      toast.success('출근 신청이 완료되었습니다.');
+      setState(true);
+    } else {
+      toast.error('이미 출근 신청을 했습니다.');
+    }
+  }
+
+  return (
+    <div>
+      <div className={styles.infoBox}>
+        <p>오늘의 근무</p>
+        <div className={styles.todays}>
+          <span className={styles.workday}>07.01</span>
+          <span>
+            08:54~18:17
+            <div className={styles.nofiBox}>퇴근완료</div>
+          </span>
+        </div>
+        <a onClick={notify} className={styles.actionBtn}>출근하기</a>
+        <Toaster />
+        <div className={styles.workTotal}>
+          <dl className={styles.workList}>
+            <dt>출근</dt>
+            <dd>09:00</dd>
+            <dt>퇴근</dt>
+            <dd>18:00</dd>
+            <dt>총 근무시간</dt>
+            <dd>9시간</dd>
+          </dl>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
--- class-viewer/pages/index.tsx
+++ class-viewer/pages/index.tsx
@@ -7,41 +7,10 @@ import React, { useState, useEffect } from 'react'
 
 const Home: NextPage = () => {
 
-  const [state, setState] = useState(false);
-
-  const notify = () => {
-    if(state === false){
-      toast.success('출근 신청이 완료되었습니다.');
-      setState(true);
-    } else {
-      toast.error('이미 출근 신청을 했습니다.');
-    }
-  }
 
   return (
     <div>
-      <div className={styles.infoBox}>
-        <p>오늘의 근무</p>
-        <div className={styles.todays}>
-          <span className={styles.workday}>07.01</span>
-          <span>
-            08:54~18:17
-            <div className={styles.nofiBox}>퇴근완료</div>
-          </span>
-        </div>
-        <a onClick={notify} className={styles.actionBtn}>출근하기</a>
-        <Toaster />
-        <div className={styles.workTotal}>
-          <dl className={styles.workList}>
-            <dt>출근</dt>
-            <dd>09:00</dd>
-            <dt>퇴근</dt>
-            <dd>18:00</dd>
-            <dt>총 근무시간</dt>
-            <dd>9시간</dd>
-          </dl>
-        </div>
-      </div>
+      index
     </div>
   )
 }
