e2e128e57be5f21f28720f5c15ae4f3e6f8f5dfc
tthugy <supermanace7@gmail.com>
2022-07-04 23:14:21 +0900

    update

--- class-viewer/pages/home.tsx
+++ class-viewer/pages/home.tsx
@@ -23,15 +23,15 @@ const Home: NextPage = () => {
       <div className={styles.infoBox}>
         <p>오늘의 근무</p>
         <div className={styles.todays}>
-          <span className={styles.workday}>07.01</span>
+          <span className={styles.workday}>07.05</span>
           <span>
-            08:54~18:17
-            <div className={styles.nofiBox}>퇴근완료</div>
+            일정없음
+            <div className={styles.nofiBox}>무일정</div>
           </span>
         </div>
         <a onClick={notify} className={styles.actionBtn}>출근하기</a>
         <Toaster />
-        <div className={styles.workTotal}>
+        {/* <div className={styles.workTotal}>
           <dl className={styles.workList}>
             <dt>출근</dt>
             <dd>09:00</dd>
@@ -40,7 +40,7 @@ const Home: NextPage = () => {
             <dt>총 근무시간</dt>
             <dd>9시간</dd>
           </dl>
-        </div>
+        </div> */}
       </div>
     </div>
   )
