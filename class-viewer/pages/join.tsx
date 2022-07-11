69354fceeb6ac4a71dd8b9e10b12791a78fd718d
tthugy <supermanace7@gmail.com>
2022-07-11 23:29:50 +0900

    update

--- class-viewer/pages/join.tsx
+++ class-viewer/pages/join.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
 import styles from '../styles/menu1.module.css'
+import Link from 'next/link'
 
 const Menu1: NextPage = () => {
   return (
     <div>
       <span className={styles.center}>회원가입</span>
+      <Link href="/">
+        <span className={styles.main}>메인화면으로</span>
+      </Link>
     </div>
   )
 }
